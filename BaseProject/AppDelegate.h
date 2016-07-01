43898f0c6bbb600b2a4c3af6ec7868c7cd57e92f
suke <quangtuan.fsb@gmail.com>
2016-07-01 10:48:12 +0700

    init

---
+++ BaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseProject
+//
+//  Created by suke on 7/1/16.
+//  Copyright © 2016 suke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseProject
+//
+//  Created by suke on 7/1/16.
+//  Copyright © 2016 suke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
